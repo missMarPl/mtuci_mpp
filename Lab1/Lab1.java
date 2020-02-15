2ee712362bcc395b64a8d29b4bbc234cdfcbfba4
missMarPl <young.miss.marpl@gmail.com>
2020-02-15 13:03:41 +0300

    lab1 and basic file for lab2

---
+++ Lab1/Lab1.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Lab1
+   {
+public static void main(String args[ ])
+      {
+      double[] coords = new double[10]; ;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input x,y,z coordinates of 1 point: ");
+        for (int i=0;i<3;i++)
+        coords[i] = in.nextDouble();
+        
+        Point3d one = new Point3d(coords[0],coords[1],coords[2]); 
+        
+        System.out.println("Input x,y,z coordinates of 2 point: ");
+        for (int i=0;i<3;i++)
+        coords[i] = in.nextDouble();
+        
+        Point3d two = new Point3d(coords[0],coords[1],coords[2]);
+
+        System.out.println("Input x,y,z coordinates of 3 point: ");
+        for (int i=0;i<3;i++)
+        coords[i] = in.nextDouble();
+        
+        Point3d three = new Point3d(coords[0],coords[1],coords[2]);        
+        
+        in.close();
+        
+        System.out.printf("Your points: \n 1: %f %f %f \n 2: %f %f %f \n 3: %f %f %f \n", one.getX(), one.getY(), one.getZ(), two.getX(), two.getY(), two.getZ(), three.getX(), three.getY(), three.getZ());
+        
+        System.out.printf("Distances: \n 1-2: %f \n 2-3: %f \n 3-1: %f \n", one.distanceTo(two), two.distanceTo(three), three.distanceTo(one));
+        
+         System.out.printf("Triangle Area: %f \n", computeArea(one,two,three));
+       }
+       
+      public static double computeArea(Point3d first, Point3d second, Point3d third)
+      {
+      double area;
+      if (!first.isEqual(second) && !second.isEqual(third) && !third.isEqual(first)) {
+      double[] sides = {first.distanceTo(second), second.distanceTo(third), third.distanceTo(first)}; 
+      double p = (sides[0]+sides[1]+sides[2])/2;
+      area = Math.sqrt(p*(p-sides[0])*(p-sides[1])*(p-sides[2]));
+      } else {
+      System.out.println("ERROR: this is NOT a trianlge! \n");
+      area=0;
+      } 
+      return area;
+      }
+   }
---
+++ Lab1/Point3d.java
@@ -0,0 +1,47 @@
+/** 
+* трёхмерный класс точки. 
+**/ 
+public class Point3d extends Point2d { 
+
+/**  координата Z **/ 
+private double zCoord; 
+/** Конструктор инициализации **/ 
+public Point3d ( double x,  double y, double z) { 
+super(x, y);
+zCoord = z; 
+} 
+/** Конструктор по умолчанию. **/ 
+public Point3d () { 
+//Вызовите конструктор с двумя параметрами и определите источник. 
+this(0, 0, 0); 
+} 
+
+/** Возвращение координаты Z **/ 
+public double getZ () { 
+return zCoord; 
+} 
+
+/**  Установка значения координаты Z. **/ 
+public void  setZ ( double val) { 
+zCoord = val; 
+} 
+
+/**  Вычисление расстояния до другой точки **/ 
+public boolean  isEqual ( Point3d otherPoint) { 
+boolean compat;
+if (this.getX()==otherPoint.getX() && this.getY()==otherPoint.getY() && this.getZ()==otherPoint.getZ())
+     compat=true;
+    else compat=false;
+
+return compat; 
+}
+
+/**  Вычисление расстояния до другой точки **/ 
+public double  distanceTo ( Point3d otherPoint) { 
+
+double distance = Math.sqrt(Math.pow((this.getX() - otherPoint.getX()), 2) + Math.pow((this.getY() - otherPoint.getY()), 2) + Math.pow((this.getZ() - otherPoint.getZ()), 2));
+
+return distance; 
+} 
+
+}
